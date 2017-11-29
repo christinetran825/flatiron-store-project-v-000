4122a08b729c6dfd96c3a8d893032db0b35b285f
Christine Tran <christinetran825@gmail.com>
2017-11-29 12:27:32 -0800

    added current_cart setter/helper methods

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,23 @@
 class CartsController < ApplicationController
 
+  helper_method :current_cart
+
+  def current_cart
+    current_user.current_cart
+  end
+
+  def current_cart=(cart)
+    current_user.current_cart = cart
+  end
+
+  def show
+    @cart = Cart.find(params[:id])
+  end
+
+  def checkout
+    cart = Cart.find(params[:id])
+    cart.checkout
+    redirect_to cart_path(cart)
+  end
+
 end
