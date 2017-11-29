56f456f39d14dd5c84d8ef18b50cedf69c1eec0e
Christine Tran <christinetran825@gmail.com>
2017-11-28 22:57:35 -0800

    create current cart method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :carts
+  belongs_to :current_cart, class_name: "Cart"
+
+  def create_current_cart
+    @new_cart = carts.create(user_id: self.id)
+    self.current_cart_id = @new_cart.id
+    save
+  end
+
+  def remove_cart
+    self.current_cart_id = nil
+    current_cart = nil
+    save
+  end
+
 end
