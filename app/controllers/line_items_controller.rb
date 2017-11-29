1dea65b340ac6a1c8b5059c2dc9ef3bad1332860
Christine Tran <christinetran825@gmail.com>
2017-11-29 12:29:01 -0800

    added create action

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,17 @@
 class LineItemsController < ApplicationController
-
+  def create
+    current_user.create_current_cart unless current_user.current_cart
+    if current_user.current_cart.items.include?(Item.find(params[:item_id]))
+      @item = current_user.current_cart.line_items.find_by(item_id: params[:item_id])
+      @item.quantity += 1
+    else
+      @item = current_user.current_cart.add_item(params[:item_id])
+    end
+    if @item.save
+      redirect_to cart_path(current_user.current_cart), {notice: "Item added to cart!"}
+    else
+      redirect_to store_path, {notice: "Unable to add item"}
+    end
+  end
 end
+ 
