50495b40551de2706130342dc0502e9880bc2124
Christine Tran <christinetran825@gmail.com>
2017-11-28 22:58:10 -0800

    add self.available items method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ActiveRecord::Base
   belongs_to :category
   has_many :line_items
+
+  def self.available_items
+    @available = []
+    self.all.collect do |item|
+      if item.inventory != 0
+        @available << item
+      end
+    end
+    @available
+  end
+
 end
